b9a6e4c4487da9bcc9b67ba272b18ebae1fbc463
Talienhyung <144995030+Talienhyung@users.noreply.github.com>
2024-03-02 18:01:50 +0100

    Drag and drop next

--- Assets/Script/DragDrop.cs
+++ Assets/Script/DragDrop.cs
@@ -1,38 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DragDrop : MonoBehaviour
 {
     public GameObject objectToDrag;
     public GameObject ObjectDragToPos;
-
     public float Dropdistance;
+    private List<Image> gameObjectList = new List<Image>();
 
     public bool isLocked;
 
     Vector2 objectinitPos;
+
     // Start is called before the first frame update
     void Start()
     {
         objectinitPos = objectToDrag.transform.position;
+        foreach (Transform child in ObjectDragToPos.transform)
+        {
+            Image subImage = child.GetComponent<Image>();
+
+            ProcessSubImage(subImage);
+        }
     }
 
     public void DragObject()
     {
-        if (!isLocked)
+        if (isLocked)
         {
-            objectToDrag.transform.position = Input.mousePosition;
+            return;
         }
+        objectToDrag.transform.position = Input.mousePosition;
+        
     }
 
     public void DropObject()
     {
-        float Distance = Vector3.Distance(objectToDrag.transform.position, ObjectDragToPos.transform.position);
-        if(Distance < Dropdistance)
+        float Distance = 80;
+        Image imageGood = null;
+        foreach (Image image in gameObjectList)
         {
-            objectToDrag.transform.position = ObjectDragToPos.transform.position;
+            float NewDistanceToImage = Vector3.Distance(objectToDrag.transform.position, image.transform.position);
+            if (NewDistanceToImage < Distance)
+            {
+                Distance = NewDistanceToImage;
+                imageGood = image;
+            }
+        }
+
+        if (Distance < Dropdistance)
+        {
+            objectToDrag.transform.position = imageGood.transform.position;
             isLocked = true;
+            return;
         }
         else
         {
@@ -46,5 +68,10 @@ public class DragDrop : MonoBehaviour
         {
             objectToDrag.transform.position = ObjectDragToPos.transform.position;
         }
+    }
+
+    void ProcessSubImage(Image subImage)
+    {
+        gameObjectList.Add(subImage);
     }
 }
